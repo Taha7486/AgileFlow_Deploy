99d45eb7a81ed36ca7e9d6f5bc19f2db70523ac9
Houssaaaaam <houssamhariss25@gmail.com>
2026-04-11 16:00:00 +0100

    init: structure Spring Boot + pom.xml + packages

---
+++ src/main/java/com/agileflow/AgileflowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.agileflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileflowBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgileflowBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/agileflow/AgileflowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agileflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgileflowBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
